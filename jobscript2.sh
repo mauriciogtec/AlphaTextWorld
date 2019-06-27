756df9e59a56631ef78276b2ea307c5c3d5528f6
Mauricio <mauriciogtec@gmail.com>
2019-06-27 00:46:13 -0700

    test2

---
+++ jobscript2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --tasks-per-node 1
+#SBATCH -p development
+#SBATCH -J gameplay2
+#SBATCH -e slurm-%j.err
+#SBATCH -t 0:120:0
+
+
+module load intel/17.0.4
+module load python3/3.6.3
+
+
+cd /work/05863/mgarciat/stampede2/AlphaTextWorld/
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
+
