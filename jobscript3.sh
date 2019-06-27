dcde9959d0626bda1bd4f7abe572dc46228ec16a
Mauricio <mauriciogtec@gmail.com>
2019-06-27 13:11:03 -0700

    fix

--- jobscript3.sh
+++ jobscript3.sh
@@ -15,10 +15,10 @@ pip3 install --user keras h5py==2.8.0
 module load phdf5
 
 export HDF5_USE_FILE_LOCKING=FALSE
-export MKL_NUM_THREADS=2
-export GOTO_NUM_THREADS=2
-export OMP_NUM_THREADS=2G
-export openmp=1
+# export MKL_NUM_THREADS=2
+# export GOTO_NUM_THREADS=2
+# export OMP_NUM_THREADS=2
+# export openmp=1
 
 cd /work/05863/mgarciat/stampede2/AlphaTextWorld/
 srun -N 1 -n 1 python3 play_remote.py --temperature 0.5 --subtrees 100 --subtree_depth 5 &
