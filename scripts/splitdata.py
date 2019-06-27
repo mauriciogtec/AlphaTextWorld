7d64a65d08f14e216899e47eeda65138f7ee1402
Mauricio <mauriciogtec@gmail.com>
2019-06-26 22:45:30 -0700

    split data

--- scripts/splitdata.py
+++ scripts/splitdata.py
@@ -26,15 +26,15 @@ for fn in trainfiles:
     json = fn.replace(".ulx", ".json")
     z8 = fn.replace(".ulx", ".z8")
     sh += "cp {}/{} {}/{}\n".format(alldir, fn, traindir, fn)
-    sh += "cp {}/{} {}/{}".format(alldir, json, traindir, json)
-    sh += "cp {}/{} {}/{}".format(alldir, z8, traindir, z8)
+    sh += "cp {}/{} {}/{}\n".format(alldir, json, traindir, json)
+    sh += "cp {}/{} {}/{}\n".format(alldir, z8, traindir, z8)
 
 for fn in testfiles:
     json = fn.replace(".ulx", ".json")
     z8 = fn.replace(".ulx", ".z8")
-    sh += "cp {}/{} {}/{}".format(alldir, fn, testdir, fn)
-    sh += "cp {}/{} {}/{}".format(alldir, json, testdir, json)
-    sh += "cp {}/{} {}/{}".format(alldir, z8, testdir, z8)
+    sh += "cp {}{} {}{}\n".format(alldir, fn, testdir, fn)
+    sh += "cp {}{} {}{}\n".format(alldir, json, testdir, json)
+    sh += "cp {}{} {}{}\n".format(alldir, z8, testdir, z8)
 
 with open(wd + "copybash.sh", "w") as fn:
     fn.write(sh)
