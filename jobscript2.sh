692c97e537f84a6ea4e9a43a6fe6bebcdd7e6918
Mauricio <mauriciogtec@gmail.com>
2019-06-27 03:33:33 -0700

    updated jjobscripts

--- jobscript2.sh
+++ jobscript2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH -N 16
-#SBATCH -n 16
+#SBATCH -n 32
 #SBATCH -c 1
 #SBATCH -p normal
 #SBATCH -J gfull
 #SBATCH -e slurm-%j.err
-#SBATCH -t 1:0:0
+#SBATCH -t 2:0:0
 
 
 module load intel/17.0.4
@@ -14,20 +14,21 @@ module load python3/3.6.3
 
 
 cd /work/05863/mgarciat/stampede2/AlphaTextWorld/
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
+python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0 &
 python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
-python3 play_remote2.py --temperature 0.5 --subtrees 100 --subtree_depth 5 --verbose 0
+wait
 
