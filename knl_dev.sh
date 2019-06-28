978527431ab30ea37f915ef0c268ea46bd81c669
Mauricio <mauriciogtec@gmail.com>
2019-06-28 12:26:10 -0700

    back to short games

--- knl_dev.sh
+++ knl_dev.sh
@@ -9,7 +9,7 @@
 
 
 module load intel/17.0.4
-module load python3/3.6.3
+module load python3/
 #pip3 install --user tensorflow==2.0.0-beta1
 pip3 install --user keras h5py==2.8.0
 module load phdf5
@@ -21,21 +21,21 @@ export OMP_NUM_THREADS=128
 # export openmp=0
 
 cd /work/05863/mgarciat/stampede2/AlphaTextWorld/
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time 5 &
-srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 125 --subtree_depth 7 --verbose 0 --min_time
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time 5 &
+srun -N 1 -n 1 python3 play_remote2.py --temperature 0.4 --subtrees 100 --subtree_depth 5 --verbose 0 --min_time
 
 wait
